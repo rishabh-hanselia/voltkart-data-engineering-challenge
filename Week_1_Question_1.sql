5fbfe94ebfa4307ef3dbeb7ff44684210ec4b7ee
Rishabh Hanselia <92868497+rishabh-hanselia@users.noreply.github.com>
2026-06-24 16:43:37 +0530

    Add files via upload

---
+++ Week_1_Question_1.sql
@@ -0,0 +1,10 @@
+WITH CompletedOrders AS (
+    SELECT order_id, order_date, customer_id, sales_rep_id, order_total
+    FROM dbo.fact_orders
+    WHERE order_status = 'Completed'
+)
+SELECT TOP 20 order_id, order_date, c.customer_name, e.employee_name AS sales_rep_name, order_total
+FROM CompletedOrders co 
+		LEFT JOIN dbo.dim_customer c ON co.customer_id = c.customer_id
+		LEFT JOIN dbo.dim_employee e ON co.sales_rep_id = e.employee_id 
+ORDER BY order_total DESC
